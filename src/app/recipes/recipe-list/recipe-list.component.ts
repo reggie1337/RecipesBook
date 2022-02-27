5249e5d28c3e8731f6263a422e82eb19344704bb
reggie <reggie.skillman34@icloud.com>
2022-02-27 10:58:17 -0500

    recipe list btn

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('tester recipe', 'this is only a test','https://www.pinterest.com/pin/231091024600867036/')
+  ];
   constructor() { }
 
   ngOnInit(): void {
