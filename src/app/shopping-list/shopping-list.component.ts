5d52d652c1d3e54e052328075cf866ff9176e7e8
reggie <reggie.skillman34@icloud.com>
2022-03-31 23:36:26 -0400

    broken

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shoppinglist.service';
 
@@ -7,19 +8,29 @@ import { ShoppingListService } from './shoppinglist.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private igChangeSub: Subscription;
   
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientChanged.subscribe(
+    this.igChangeSub = this.slService.ingredientChanged
+   .subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
     );
+    onEditItem(index: number) {
+      this.slService.startedEditing.next(index);
+
+    }
   }
+  ngOnDestroy(): void {
+    this.igChangeSub.unsubscribe();
+  }
+  
 
 }
