332779a44fc3abaf51ba1ab6caaee72ce1e7c846
reggie <reggie.skillman34@icloud.com>
2022-03-31 22:39:16 -0400

    formsIncoming?

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shoppinglist.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,8 +9,6 @@ import { ShoppingListService } from '../shoppinglist.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild ('nameInput') nameInputRef:ElementRef;
-  @ViewChild ('amountInput') amountInputRef:ElementRef;
 
 
 
@@ -18,10 +17,9 @@ export class ShoppingEditComponent implements OnInit {
   ngOnInit(): void {
   }
   
-  onAddItem (){
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient (ingName, ingAmount);
+  onAddItem (form : NgForm){
+    const value = form.value;
+    const newIngredient = new Ingredient (value.name, value.amount);
     this.slService.addIngredient(newIngredient);
   }
 }
